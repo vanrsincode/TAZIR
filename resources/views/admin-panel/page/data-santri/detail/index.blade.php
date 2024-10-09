53440a0f5c707d818aa9c18d4bcdf26dbfdf7154
vanrsincode <vanrsincode@gmail.com>
2024-10-10 01:38:49 +0700

    update: detail-santri

--- resources/views/admin-panel/page/data-santri/detail/index.blade.php
+++ resources/views/admin-panel/page/data-santri/detail/index.blade.php
@@ -19,9 +19,9 @@
             <p class="section-lead">Components relating to users, lists of users and so on.</p>
 
             <div class="row">
-                <div class="col-6 col-sm-12 col-lg-6">
+                <div class="col-sm-12 col-lg-6">
                     <div class="card author-box card-success">
-                        <div class="card-body">
+                        <div class="card-body mt-2">
                             <div class="author-box-left">
                                 <img alt="image" src="{{ asset('adm-panel') }}/assets/img/avatar/avatar-1.png"
                                     class="rounded-circle author-box-picture">
@@ -32,7 +32,7 @@
                             </div>
                             <div class="author-box-details">
                                 <div class="author-box-name">
-                                    <a href="#">Hasan Basri</a>
+                                    <a href="#" class="text-success">Hasan Basri</a>
                                 </div>
                                 <div class="author-box-job">Kelas</div>
                                 <div class="author-box-description">
@@ -67,16 +67,16 @@
 
                 </div>
 
-                <div class="col-6 col-sm-12 col-lg-6">
-                    <div class="card profile-widget shadow-sm">
+                <div class="col-sm-12 col-lg-6">
+                    <div class="card profile-widget shadow-sm mt-0 card-success">
                         <div class="profile-widget-header text-center">
-                            <div class="profile-widget-items">
+                            <div class="profile-widget-items mt-2">
                                 <div class="profile-widget-item">
                                     <div class="profile-widget-item-label">Pelanggaran</div>
                                     <div class="profile-widget-item-value">187</div>
                                 </div>
                                 <div class="profile-widget-item">
-                                    <div class="profile-widget-item-label">Tunggakan Denda</div>
+                                    <div class="profile-widget-item-label">Tunggakan</div>
                                     <div class="profile-widget-item-value">6,8K</div>
                                 </div>
                                 <div class="profile-widget-item">
@@ -93,11 +93,12 @@
                                     <div class="slash"></div> Kelas
                                 </div>
                                 <div class="text-muted d-inline font-weight-normal">
-                                <p>Kami sampaikan bahwa <strong>Nama</strong> telah tercatat melakukan
-                                    beberapa pelanggaran yang memerlukan perhatian. Total denda yang terakumulasi akibat
-                                    pelanggaran tersebut adalah <strong>Rp.000000</strong>.</p>
-                                <p><strong>Konsekuensi:</strong> Santri ini tidak dapat mengambil rapor atau mengikuti ujian
-                                    tamrin dan ujian akhir apabila denda pelanggarannya tidak dilunaskan.</p>
+                                    <p>Kami sampaikan bahwa <strong>Nama</strong> telah tercatat melakukan
+                                        beberapa pelanggaran yang memerlukan perhatian. Total denda yang terakumulasi akibat
+                                        pelanggaran tersebut adalah <strong>Rp.000000</strong>.</p>
+                                    <p><strong>Konsekuensi:</strong> Santri ini tidak dapat mengambil rapor atau mengikuti
+                                        ujian
+                                        tamrin dan ujian akhir apabila denda pelanggarannya tidak dilunaskan.</p>
                                 </div>
                             </div>
                             <div class="mt-3 mb-3">
@@ -117,53 +118,77 @@
 
         <!-- Section 2: Tabel Pelanggaran Belum Di Tebus -->
         <div class="row mt-4">
-            <div class="col-lg-12">
+            <div class="col-12">
                 <div class="card irounded-1 shadow">
-                    <div class="card-header">
-                        <h4 class="text-dark">Pelanggaran Belum Di Tebus</h4>
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <p class="card-title font-weight-bold text-dark" style="white-space: nowrap;">Tabel Pelanggaran</p>
+                        {{-- <button type="button" class="Btn" id="create" >
+                            <div class="btn-ico-plus">
+                                <span class="fas fa-plus"></span>
+                            </div>
+                            <div class="btn-text">Tambah Data</div>
+                        </button> --}}
                     </div>
                     <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped" id="detail-santri-tbl1">
-                                <thead class="bg-secondary text-dark">
-                                    <tr style="text-align: center;">
-                                        <th>#</th>
-                                        <th>Pelanggaran</th>
-                                        <th>Tanggal</th>
-                                        <th>Foto</th>
-                                        <th>Denda</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Section 3: Tabel Pelanggaran Sudah Di Tebus -->
-        <div class="row mt-4">
-            <div class="col-lg-12">
-                <div class="card irounded-1 shadow">
-                    <div class="card-header">
-                        <h4 class="text-dark">Pelanggaran Di Tebus</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped" id="detail-santri-tbl2">
-                                <thead class="bg-secondary text-dark">
-                                    <tr style="text-align: center;">
-                                        <th>#</th>
-                                        <th>Pelanggaran</th>
-                                        <th>Tanggal</th>
-                                        <th>Foto</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                            </table>
+                        <ul class="nav nav-pills" id="userTabs">
+                            <li class="nav-item">
+                                <a class="nav-link nav-link-blm-tebus active" data-toggle="tab" href="#tab-blm-tebus">Belum Ditebus</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link nav-link-sdh-tebus" data-toggle="tab"
+                                    href="#tab-sdh-tebus">Sudah Ditebus</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane fade tab-pane-blm-tebus show active" id="tab-blm-tebus" role="tabpanel">
+                                <div class="table-responsive mt-3 tab-blm-tebus">
+                                    <table class="table table-striped dt-responsive nowrap" id="blm-tebus-tbl"
+                                        style="width: 100%;">
+                                        <thead class="bg-secondary text-dark">
+                                            <tr style="text-align-last: center;">
+                                                <th>#</th>
+                                                <th>Pelanggaran</th>
+                                                <th>Tanggal</th>
+                                                <th>Foto</th>
+                                                <th>Denda</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr style="text-align-last: center;">
+                                                <td>#</td>
+                                                <td>#</td>
+                                                <td>#</td>
+                                                <td>#</td>
+                                                <td>#</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade tab-pane-sdh-tebus" id="tab-sdh-tebus" role="tabpanel">
+                                <div class="table-responsive mt-3 tab-sdh-tebus">
+                                    <table class="table table-striped dt-responsive nowrap" id="sdh-tebus-tbl" style="width: 100%;">
+                                        <thead class="bg-secondary text-dark">
+                                            <tr style="text-align: center;">
+                                                <th>#</th>
+                                                <th>Pelanggaran</th>
+                                                <th>Tanggal</th>
+                                                <th>Foto</th>
+                                                <th>Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr style="text-align-last: center;">
+                                                <td>#</td>
+                                                <td>#</td>
+                                                <td>#</td>
+                                                <td>#</td>
+                                                <td>#</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
